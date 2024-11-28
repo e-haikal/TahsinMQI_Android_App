94f3e895d90d438886dfe7e8839310f6920a981e
haikal <asmanhaikal@gmail.com>
2024-11-28 22:02:05 +0700

    feat: adding splash screen feature

---
+++ app/src/main/java/com/siaptekno/tahsinmqi/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.siaptekno.tahsinmqi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashDisplayLength: Long = 3000 // duration on millisecond (3000 ms = 3 seconds)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        // delaying and directing to main activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, splashDisplayLength)
+    }
+}
