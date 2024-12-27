961f2eb8932cb81c211fb3ac17575c58de263ec9
haikal <asmanhaikal@gmail.com>
2024-12-27 14:34:43 +0700

    feat(jadwal): adding jadwal sholat layout

---
+++ app/src/main/java/com/siaptekno/tahsinmqi/ui/jadwal/JadwalSholatFragment.kt
@@ -0,0 +1,31 @@
+package com.siaptekno.tahsinmqi.ui.jadwal
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.siaptekno.tahsinmqi.R
+
+class JadwalSholatFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = JadwalSholatFragment()
+    }
+
+    private val viewModel: JadwalSholatViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_jadwal_sholat, container, false)
+    }
+}
---
+++ app/src/main/java/com/siaptekno/tahsinmqi/ui/jadwal/JadwalSholatViewModel.kt
@@ -0,0 +1,7 @@
+package com.siaptekno.tahsinmqi.ui.jadwal
+
+import androidx.lifecycle.ViewModel
+
+class JadwalSholatViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
