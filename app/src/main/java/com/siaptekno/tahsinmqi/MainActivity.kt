246c43d2007fcde2fab905392fb993dbc9bff1c7
haikal <asmanhaikal@gmail.com>
2024-12-31 10:37:21 +0700

    fix(quran-detail): adding padding so the list can show fully

--- app/src/main/java/com/siaptekno/tahsinmqi/MainActivity.kt
+++ app/src/main/java/com/siaptekno/tahsinmqi/MainActivity.kt
@@ -21,10 +21,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
