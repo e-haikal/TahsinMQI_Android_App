b38043e1bd6463b214cb041d8696aff0218aa763
haikal <asmanhaikal@gmail.com>
2025-01-07 11:46:13 +0700

    feat(material-hukum bacaan): adding initial logic

--- app/src/main/java/com/siaptekno/tahsinmqi/ui/materi/chapter_four/fragment/NunMatiFragment.kt
+++ app/src/main/java/com/siaptekno/tahsinmqi/ui/materi/chapter_four/fragment/NunMatiFragment.kt
@@ -1,5 +1,6 @@
 package com.siaptekno.tahsinmqi.ui.materi.chapter_four.fragment
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,12 @@ class NunMatiFragment : Fragment() {
     private var _binding : ItemMateriNunMatiBinding? = null
     private val binding get() = _binding!!
 
+    private var mediaPlayer: MediaPlayer? = null
+    private var mediaPlayer2: MediaPlayer? = null
+    private var mediaPlayer3: MediaPlayer? = null
+    private var mediaPlayer4: MediaPlayer? = null
+    private var mediaPlayer5: MediaPlayer? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +35,14 @@ class NunMatiFragment : Fragment() {
         // Inflate the layout using the binding class
         _binding = ItemMateriNunMatiBinding.inflate(inflater, container, false)
         return binding.root
+
+        val playButton1 = binding.ivBtnPlay1
+        val playButton2 = binding.ivBtnPlay2
+        val playButton3 = binding.ivBtnPlay3
+        val playButton4 = binding.ivBtnPlay4
+        val playButton5 = binding.ivBtnPlay5
+
+        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.nun_mati_audio)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
